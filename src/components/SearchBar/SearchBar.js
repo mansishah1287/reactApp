0075e0ac1b6257fb7d78a109fb809436d4951018
Mansi Shah <mshah@Mansis-MBP.fios-router.home>
2019-01-07 22:34:33 -0600

    Initial structure for react app

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './SearchBar.css';
+
+function SearchBar(props) {
+    return (
+        <div className="SearchBar">
+            <form className="SearchBar_Form">
+                <input className="SearchBar__Input"
+                       value={ props.value }
+                       onChange={ props.onChange } />
+                <button className="SearchBar__Button" onClick={ props.onClick }>search</button>
+            </form>
+        </div>
+    );
+}
+
+export default SearchBar;
---
+++ src/components/StockListItem/StockListItem.js
@@ -0,0 +1,37 @@
+import React, { Component} from "react";
+import './StockListItem.css';
+
+// const StockListItem = (stock, props) => {
+//     function handleClick(e) {
+//         console.log("This is a test");
+//     }
+//   return (
+//     <li className="StockListItem" onClick={ handleClick }>
+//       <div className="StockListItem_Symbol"><span>Stock: </span>{ stock.symbol }</div>
+//       <div className="StockListItem_Price"><span>Price: </span>${ parseInt(stock.price).toFixed(2) }</div>
+//       <div className="StockListItem_Volume"><span>Vol: </span>{ stock.volume }</div>
+//       <div className="StockListItem_Time"><span>Time: </span>{ stock.timestamp }</div>
+//     </li>
+//   )
+// }
+
+class StockListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(e) {
+        console.log("I am click");
+    }
+    render() {
+        return (
+            <li className="StockListItem" onClick={ this.handleClick }>
+              <div className="StockListItem_Symbol"><span>Stock: </span>{this.props.symbol}</div>
+              <div className="StockListItem_Price"><span>Price: </span>${parseInt(this.props.price).toFixed(2)}</div>
+              <div className="StockListItem_Volume"><span>Vol: </span>{this.props.volume}</div>
+              <div className="StockListItem_Time"><span>Time: </span>{this.props.timestamp}</div>
+            </li>
+        );
+    }
+}
+export default StockListItem;
